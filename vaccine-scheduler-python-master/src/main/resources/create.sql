ab91e9718816810342a1166b5bed781280805b44
sourya7496 <124980167+sourya7496@users.noreply.github.com>
2024-05-25 21:08:06 -0700

    Add files via upload

--- vaccine-scheduler-python-master/src/main/resources/create.sql
+++ vaccine-scheduler-python-master/src/main/resources/create.sql
@@ -1,18 +1,39 @@
-CREATE TABLE Caregivers (
-    Username varchar(255),
-    Salt BINARY(16),
-    Hash BINARY(16),
-    PRIMARY KEY (Username)
+CREATE TABLE Caregiver (
+    username VARCHAR(255) PRIMARY KEY,
+    salt BINARY(16),
+    hash BINARY(16)
 );
 
-CREATE TABLE Availabilities (
-    Time date,
-    Username varchar(255) REFERENCES Caregivers,
-    PRIMARY KEY (Time, Username)
+CREATE TABLE Patients (
+    username VARCHAR(255) PRIMARY KEY,
+    salt BINARY(16),
+    hash BINARY(16)
 );
 
 CREATE TABLE Vaccines (
-    Name varchar(255),
-    Doses int,
-    PRIMARY KEY (Name)
+    Name VARCHAR(255) PRIMARY KEY,
+    Doses INT
 );
+
+CREATE TABLE Availabilities (
+    date DATE,
+    username VARCHAR(255) REFERENCES Caregiver
+    PRIMARY KEY (date, username),
+);
+
+CREATE TABLE Appointments (
+    appointment_id INT PRIMARY KEY,
+    date DATE,
+    cid VARCHAR(255),
+    pid VARCHAR(255),
+    vaccine_name VARCHAR(255),
+    FOREIGN KEY (cid) REFERENCES Caregiver(username),
+    FOREIGN KEY (pid) REFERENCES Patients(username),
+    FOREIGN KEY (vaccine_name) REFERENCES Vaccines(Name)
+);
+
+
+
+
+
+
